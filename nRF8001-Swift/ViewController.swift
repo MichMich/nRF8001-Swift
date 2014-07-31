4ead610b57eb9e7f0944c58172fa54fd4330d364
Michael Teeuw <michael@xonaymedia.nl>
2014-07-31 14:30:12 +0200

    Initial Commit

---
+++ nRF8001-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nRF8001-Swift
+//
+//  Created by Michael Teeuw on 31-07-14.
+//  Copyright (c) 2014 Michael Teeuw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ nRF8001-SwiftTests/nRF8001_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  nRF8001_SwiftTests.swift
+//  nRF8001-SwiftTests
+//
+//  Created by Michael Teeuw on 31-07-14.
+//  Copyright (c) 2014 Michael Teeuw. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class nRF8001_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
